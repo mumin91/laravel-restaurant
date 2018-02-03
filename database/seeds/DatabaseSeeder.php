90e6e48e856a004f4e46254e76c02ed35700970f
Mumin <r.muminur@gmail.com>
2018-02-03 20:56:52 +0600

    submission copy

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        // $this->call(UsersTableSeeder::class);
-        $this->call(MenuTableSeeder::class);
+        //$this->call(MenuTableSeeder::class);
         $this->call(AdminTableSeeder::class);
         $this->call(CategoryTableSeeder::class);
 
